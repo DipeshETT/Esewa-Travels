87cda53a7d000923bc9aed0b6bb158407b0c0758
DipeshETT <dipesh.lama@esewatravels.com>
2026-09-15 11:48:38 +0545

    Update EsewaTravels binary target to version 1.0.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EsewaTravels",
-            url: "https://github.com/DipeshETT/Esewa-Travels/releases/download/v1.0.0/EsewaTravels.xcframework.zip",
-            checksum: "d2ccf4c3507a52810c19e93a135c18997480208e5552430255d985048817dc7b"
+            url: "https://github.com/DipeshETT/Esewa-Travels/releases/download/v1.0.1/EsewaTravels.xcframework.zip",
+            checksum: "3f8874a6c402ae081baafbe94d856e3b60e06e84332590f0508d44c9accf7673"
         )
     ]
 )
